03e14928e575c698595161907f352fbfc2ccc5ae
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-09-01 12:58:35 -0500

    Added a test for my math fuction

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,36 @@
 
 function add($a, $b)
 {
+	if(is_numeric($a)&& is_numeric($b)){
     return $a + $b;
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+    if(is_numeric($a)&& is_numeric($b)){
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    if(is_numeric($a)&& is_numeric($b)){
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    if(is_numeric($a)&& is_numeric($b)){
+    return $a / $b;
 }
 
 // Add code to test your functions here
+function modulus($a,$b)
+{
+	return $a % $b;
+}
+	echo '5 + 4 = ' . add(5, 4) . PHP_EOL;
+	echo '40 - 10 = ' . subtract(37, 15) . PHP_EOL;
+	echo '10 * 30 = ' . multiply(12, 95) . PHP_EOL;
+	echo '10 / 0 = ' . divide(27, 0) . PHP_EOL; 
+	echo '6 % 12 =' .modulus(57, 12) . PHP_EOL;
+
