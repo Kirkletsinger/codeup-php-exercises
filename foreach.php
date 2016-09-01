4de3d3c217b87b8fd4ecfa3f7c615e2467501ff0
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-09-01 09:55:04 -0500

    finished foreach_books,foreach and debug exercise. stared on breack_continue exercise and finished step on. also started switch exersise.

--- foreach.php
+++ foreach.php
@@ -1,12 +1,15 @@
 <?php
 	$things = array('Sgt. Pepper', "11", null, array( 1,2,3),3.14 , "12 + 7", false, (string) 11 );
-	foreach ($things as $stuff) {
-		if (is_array($stuff)) {
-			foreach ($stuff as $thingies) {
-				echo "$thingies" . PHP_EOL;
+	foreach ($things as $thing) {
+		echo "The type is" .gettype($things) ."The value is: "
+		if (is_array($thing)) {
+				echo "Array: "
+			foreach ($thing as $thingies) {
+				echo $thingies . " ";
 				
 			}
 		} else {
-			echo "$stuff" . PHP_EOL;
+			echo $thing;
 		}
+		echo PHP_EOL;
 	}
