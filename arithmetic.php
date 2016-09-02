f408d82ee6a796206dae8591afc415c21b89eb22
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-09-02 09:24:56 -0500

    Stated on Technology-companies exercise. sorted companies by name.

--- arithmetic.php
+++ arithmetic.php
@@ -1,37 +1,57 @@
 <?php
-
-function add($a, $b)
-{
+$a = 2;
+$b = 10;
+function add($a, $b){
 	if(is_numeric($a)&& is_numeric($b)){
-    return $a + $b;
+    	return $a + $b;
+	}else{
+		return "ERROR: Both arguments must be numbers\n";
+
+	}
 }
 
-function subtract($a, $b)
-{
+function subtract($a, $b){
     if(is_numeric($a)&& is_numeric($b)){
-    return $a - $b;
+    	return $a - $b;
+	}else{
+		return "ERROR: Both arguments must be numbers\n";
+
+	}
 }
 
-function multiply($a, $b)
-{
+function multiply($a, $b){
     if(is_numeric($a)&& is_numeric($b)){
-    return $a * $b;
+    	return $a * $b;
+	}else{
+		return "ERROR: Both arguments must be numbers\n";
+	}
 }
-
-function divide($a, $b)
-{
+function divide($a, $b){
     if(is_numeric($a)&& is_numeric($b)){
-    return $a / $b;
+    	return $a / $b;
+	}else{
+		return "ERROR: Both arguments must be numbers\n";
+
+	}
 }
 
+function modulus($a,$b){
+	if(is_numeric($a)&& is_numeric($b)){
+		return $a % $b;
+	}else{
+		return "ERROR: Both arguments must be numbers\n";
+
+	}
+}
 // Add code to test your functions here
-function modulus($a,$b)
-{
-	return $a % $b;
-}
+function Test($a,$b)
+	{
+
 	echo '5 + 4 = ' . add(5, 4) . PHP_EOL;
-	echo '40 - 10 = ' . subtract(37, 15) . PHP_EOL;
-	echo '10 * 30 = ' . multiply(12, 95) . PHP_EOL;
-	echo '10 / 0 = ' . divide(27, 0) . PHP_EOL; 
-	echo '6 % 12 =' .modulus(57, 12) . PHP_EOL;
+	echo '40 - 10 = ' . subtract(40, 10) . PHP_EOL;
+	echo '10 * 30 = ' . multiply(10, 30) . PHP_EOL;
+	echo '10 / 0 = ' . divide(10, 0) . PHP_EOL; 
+	echo '6 % 12 =' .modulus(6, 12) . PHP_EOL;
+	}
+}
 
