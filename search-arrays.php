a2ada51bca4b537515ac7780e6bdb87f092d674b
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-09-05 18:05:59 -0500

    added search-array, merge-array, and list_with_commas exercises. all complete.

--- search-arrays.php
+++ search-arrays.php
@@ -1,6 +1,25 @@
 <?php
-
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
-
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
-
+function findArray($a, $person)
+{
+    $result = array_search($person, $a);
+    if ($result !== FALSE) {
+        return TRUE.PHP_EOL;
+    } else {
+        return FALSE.PHP_EOL;
+    }
+}
+echo findArray($names, 'Tina');
+echo findArray($names, 'Bob');
+function arrayMatch ($names, $compare)
+{
+    $count = 0;
+    foreach ($names as $person) {
+        if (findArray($compare, $person)) {
+            $count++;
+        }
+    } 
+    return $count;
+}
+echo arrayMatch($names, $compare);
